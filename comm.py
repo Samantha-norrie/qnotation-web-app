8ea508de87797d429dcfaa22516ceaeb120f405a
Samantha Norrie <49314677+Samantha-norrie@users.noreply.github.com>
2024-09-12 10:46:56 -0700

    minor cors fix part 2

--- comm.py
+++ comm.py
@@ -8,9 +8,9 @@ from Errors import TooManyQubitsError, TooManyQubitsForTensorError, InvalidGateE
 import os
 
 
-app = Flask(__name__, static_folder='../qnotation_node/build')
+app = Flask(__name__)
 
-CORS(app, resources={r"/*":{"origins": "https://qnotation.vercel.app/"}})
+CORS(app, resources={r"/*":{"origins": "https://qnotation.vercel.app"}})
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
