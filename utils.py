ed4d64818aa57349c6ad58cc985f1e2482c5bd37
Samantha Norrie <49314677+Samantha-norrie@users.noreply.github.com>
2025-05-12 16:18:23 -0700

    tab issue on heroku debug

--- utils.py
+++ utils.py
@@ -8,11 +8,17 @@ import copy
 from errors import GateNotImplementedError
 from GateInformation import GateInformation
 
-CIRCUIT_GATE_LOOP = '''\tgate_list = []\n
-\tfor gate in qc.data:\n\
-\tqubit_indices = [qc.find_bit(q).index for q in gate.qubits]
-\tgate_list.append({"name": gate.name, "qubit_indices": qubit_indices, "params": gate.params})\n
-\tprint([qc.num_qubits, gate_list])\n'''
+# CIRCUIT_GATE_LOOP = '''\tgate_list = []\n
+# \tfor gate in qc.data:\n\
+# \tqubit_indices = [qc.find_bit(q).index for q in gate.qubits]
+# \tgate_list.append({"name": gate.name, "qubit_indices": qubit_indices, "params": gate.params})\n
+# \tprint([qc.num_qubits, gate_list])\n'''
+
+CIRCUIT_GATE_LOOP = '''    gate_list = []\n
+    for gate in qc.data:\n\
+        qubit_indices = [qc.find_bit(q).index for q in gate.qubits]
+        gate_list.append({"name": gate.name, "qubit_indices": qubit_indices, "params": gate.params})\n
+        print([qc.num_qubits, gate_list])\n'''
 
 CONTROL = "CONTROL"
 TARGET = "TARGET"
